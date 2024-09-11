6ca35bcf06667c81a30cc1581bc325fff555e233
Heidrick <heidrick@vikasbansalsMBP.lan>
2024-09-12 01:03:23 +0530

    Added-All-File

---
+++ src/main/java/com/example/EnterpriseSpring/EnterpriseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnterpriseSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnterpriseSpring/EnterpriseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnterpriseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
