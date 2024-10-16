e4a47b6b37eedbe4ea01cb9aaf0e63f233d4d189
saksham0310 <saksham0071@gmail.com>
2024-10-17 01:01:15 +0530

    First-API

--- src/main/java/com/example/EnterpriseSpring/controllers/test.java
+++ src/main/java/com/example/EnterpriseSpring/controllers/test.java
@@ -1,4 +1,13 @@
 package com.example.EnterpriseSpring.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class test {
+
+    @GetMapping("value")
+    public String value(){
+        return "Hello Worls";
+    }
 }
